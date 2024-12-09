108404f93d236bafaf4ba144f782e7b4bab959b6
samkons <konzisamuel@gmail.com>
2024-12-09 12:46:09 +0100

    add checks for client id

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,8 +10,8 @@ const { cognitoAuthority, client_id, localURL } = config;
 
 const url_test = `${localURL}callback`
 console.log(url_test);
-console.log("authority:--->", cognitoAuthority)
-console.log(client_id)
+console.log("authority:--->", cognitoAuthority, "//")
+console.log("str-",client_id,"-end")
 
 const cognitoAuthConfig = {
   authority: cognitoAuthority,
